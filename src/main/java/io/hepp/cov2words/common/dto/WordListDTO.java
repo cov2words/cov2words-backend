5e5f0ac6b990827248f0e9a9bc7e31b695dd7e8b
Thomas Hepp <thomas.hepp@originstamp.com>
2020-03-29 15:24:55 +0200

    Reset german wordlist

--- src/main/java/io/hepp/cov2words/common/dto/WordListDTO.java
+++ src/main/java/io/hepp/cov2words/common/dto/WordListDTO.java
@@ -15,6 +15,6 @@ import java.util.List;
 @Setter
 @ToString
 public class WordListDTO implements Serializable {
-    @JsonProperty(value = "term")
+    @JsonProperty(value = "words")
     private List<String> terms;
 }
